05f44a4297b96de05ccb40c0e155530db9bfa1e9
preetidipto <152097771+preetidipto@users.noreply.github.com>
2024-04-16 16:06:55 +0600

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,8 @@
+//bx slider
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    jQuery('.main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth:768,
+    });
+  });
